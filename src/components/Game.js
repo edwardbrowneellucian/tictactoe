3d29bcbec516c5667549e3df767462d5e7f21c57
Edward Browne <edward.browne@ellucian.com>
2022-12-30 11:18:08 -0330

    added Draw messages

--- src/components/Game.js
+++ src/components/Game.js
@@ -24,11 +24,13 @@ class Game extends React.Component {
     handleClick(i) {
         const history = this.state.history.slice(0,this.state.stepNumber + 1);    
         const current = history[history.length - 1];    
-        const squares = current.squares.slice();    
+        const squares = current.squares.slice();
+           
         if (calculateWinner(squares) || squares[i]) {
           return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
+       
         this.setState({
           history: history.concat([{        
             squares: squares,      }]),
@@ -41,7 +43,8 @@ class Game extends React.Component {
         const history = this.state.history;    
         const current = history[this.state.stepNumber];    
         const winner = calculateWinner(current.squares);    
-        
+        const winnerline = calculateWinnerLine(current.squares);
+
         const moves = history.map((step, move) => {      
             const desc = move ? 
             'Go to move #' + move : 
@@ -58,9 +61,13 @@ class Game extends React.Component {
         let status;    
         
         if (winner) { 
-            status = 'Winner: ' + winner;    
-        } else {      
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');    
+            status = 'Winner: ' + winner +' ' + winnerline;    
+        } else { 
+            if (this.state.stepNumber === 9){
+              status = 'Draw'
+            } else {    
+              status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');   
+            } 
         }
 
       return (
@@ -95,6 +102,28 @@ class Game extends React.Component {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ) {
         return squares[a];
+        //return lines[i];
+      }
+    }
+    return null;
+  }
+
+  function calculateWinnerLine(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ) {
+        //return squares[a];
+        return lines[i];
       }
     }
     return null;
